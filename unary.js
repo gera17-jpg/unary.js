30f1cfcf460b474e4b4f2755ec505a3902d64b8d
Gemma Yubos <142143749+gera17-jpg@users.noreply.github.com>
2024-07-03 15:03:33 +0800

    Create unary.js

---
+++ unary.js
@@ -0,0 +1,12 @@
+/* Increement Unary Operator*/
+let y = 20;
+let inc = y++;
+console.log(inc); //20
+inc=y;
+console.log(inc);  //21
+
+let g = 10;
+let v = ++g;
+console.log(v)  // 11
+
+//Same with Decreement
